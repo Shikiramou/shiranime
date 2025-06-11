f92e7650c254eec297f2c2af334e5e0f04d175c6
erzahary <akun93036@gmail.com>
2025-06-11 22:51:18 +0800

    update fix error

--- src/app/libs/api-libs.ts
+++ src/app/libs/api-libs.ts
@@ -1,11 +1,9 @@
-
-
 export const getAnimeResponse = async (
-source: string, query: string, p0: { next: { revalidate: number; }; }) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${source}?${query}`);
-    
-
-    const data = await response.json();
-    return data;
-  
+  source: string, 
+  query: string, 
+  options: { next: { revalidate: number } }
+) => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/${source}?${query}`, options);
+  const data = await response.json();
+  return data;
 };
