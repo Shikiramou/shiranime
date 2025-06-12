3727d89d2fb78c8f7151ad701806a4f222bad34f
erzahary <akun93036@gmail.com>
2025-06-12 09:04:09 +0800

    100% no error

--- src/app/Anime/[id]/page.tsx
+++ src/app/Anime/[id]/page.tsx
@@ -2,17 +2,16 @@ import { getAnimeResponse } from "@/app/libs/api-libs"
 
 interface PageProps {
   params: {
-    id: any
+    id: string 
   }
 }
 
 export default async function Page({ params }: PageProps) {
+  const { id } = params 
   
-  const { id } = await params
-  
-  // Kemudian gunakan id untuk fetch data
   const response = await getAnimeResponse(`anime/${id}`)
   const anime = response.data
+  
   return (
     <>
       <div>{anime.title}</div>
