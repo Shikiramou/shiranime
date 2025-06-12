a25f0692c5f9703b7004b1491baf896c7821eae9
Shikiramou <hrry2806@gmail.com>
2025-06-12 17:25:15 +0800

    tes

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,19 +1,12 @@
 import { getAnimeResponse } from "@/app/libs/api-libs";
 import VideoPlayer from "@/components/Utilities/VideoPlayer";
 
-
-
-export default async function Page({ params }: any) {
- 
+export default async function Page({ params }: { params: { id: string } }) {
   const response = await getAnimeResponse(`anime/${params.id}`);
   
- 
   return (
     <>
-    
-      
-        <VideoPlayer youtubeId={response?.data.trailer.youtube_id} />
-      
+      <VideoPlayer youtubeId={response?.data.trailer.youtube_id} />
     </>
   );
 }
