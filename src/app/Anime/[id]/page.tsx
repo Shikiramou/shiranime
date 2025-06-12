2a5dc4a320f025654049228fc82c7bd86a623662
erzahary <akun93036@gmail.com>
2025-06-12 09:10:08 +0800

    asli no error

--- src/app/Anime/[id]/page.tsx
+++ src/app/Anime/[id]/page.tsx
@@ -1,20 +1,24 @@
-import { getAnimeResponse } from "@/app/libs/api-libs"
+import { getAnimeResponse } from "@/app/libs/api-libs";
 
+// Interface yang kompatibel dengan tipe Next.js
 interface PageProps {
-  params: {
-    id: string 
-  }
+  params: Promise<{
+    id: string;
+  }>;
 }
 
 export default async function Page({ params }: PageProps) {
-  const { id } = params 
-  
-  const response = await getAnimeResponse(`anime/${id}`)
-  const anime = response.data
-  
+  // Await the Promise to get params
+  const resolvedParams = await params;
+  const { id } = resolvedParams;
+
+  // Fetch data
+  const response = await getAnimeResponse(`anime/${id}`);
+  const anime = response.data;
+
   return (
     <>
       <div>{anime.title}</div>
     </>
-  )
+  );
 }
